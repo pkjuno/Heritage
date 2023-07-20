23b2056f37386146fd058c0f752921ea520e235b
DESKTOP-QIBNG2D\pkjun <pkjuno@nate.com>
2023-07-20 18:08:57 +0900

    최초 springBoot & vue 프로젝트 PUSH

---
+++ src/main/java/com/project/projectheritage/ProjectHeritageApplication.java
@@ -0,0 +1,13 @@
+package com.project.projectheritage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHeritageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectHeritageApplication.class, args);
+    }
+
+}
